5a2191ce709fa13d4fc2a855ae5a0323bacc48ba
Adixshan <138473539+Adixshan@users.noreply.github.com>
2025-01-25 23:29:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# Replace with your actual credentials
-REDDIT_CLIENT_ID = "IZ7fUhqsrUqQv28njC1CaQ"
-REDDIT_SECRET = "EIYTRjHq6scWL40o_V5_xz5k9EWTEA"
+# I have placed changed credentials for security purpose
+REDDIT_CLIENT_ID = "IZ7fUhq28njC1CaQ"
+REDDIT_SECRET = "EIYTRjHq6so_V5_xz5k9EWTEA"
 REDDIT_USERNAME = "Adicshan"
 REDDIT_PASSWORD = "Raushankr@8816616"
 USER_AGENT = "MyRedditBot v1.0 by /u/Adicshan"
 
-GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"  # Update as per Groq Cloud's documentation
-GROQ_API_KEY = "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"
+GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completons"  # Update as per Groq Cloud's documentation
+GROQ_API_KEY = "gsk_NbdRe7L1fXOLALOaDzQnWGdyb3FYvM5zI1btoCmD0sNaRuU"
